ebd05361929f73910db9dd5f4771ed03da81a892
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-16 11:21:08 -0500

    added second function with new variables

--- functions-2.js
+++ functions-2.js
@@ -1,3 +1,9 @@
+function addToAll() {
+    x = x + 1;
+    y = y + 1;
+    z = z + 1;
+}
+
 let x = 5;
 x;
 
@@ -17,3 +23,11 @@ x =  x + 100;
 
 addToX();
 x 
+let y = 1
+let z = 1
+
+addToAll();
+
+x
+y
+z
